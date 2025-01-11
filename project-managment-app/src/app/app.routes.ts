a376a20f170644f1030e8cc435c7b6bc5cf1e04d
TumeloGeorge <107951672+TumeloGeorge@users.noreply.github.com>
2025-01-11 21:21:28 +0200

    modified:   project-managment-app/src/app/app.component.html 	modified:   project-managment-app/src/app/app.routes.ts 	modified:   project-managment-app/src/app/dashboard/dashboard.component.html 	modified:   project-managment-app/src/app/dashboard/dashboard.component.ts

--- project-managment-app/src/app/app.routes.ts
+++ project-managment-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule} from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DashboardComponent},
+    // routes for components
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- project-managment-app/src/app/dashboard/dashboard.component.ts
+++ project-managment-app/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProjectService } from '../project.service';
+import { Project } from '../models';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -8,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  projects: Project[] = [];
 
+  constructor(private projectService: ProjectService) {
+    this.projects = this.projectService.getProjects();
+  }
+
+  addProject(): void{
+    const newProject: Project = {
+      id: Date.now(),
+      name: `Project ${this.projects.length + 1}`,
+      description: `This is project ${this.projects.length + 1} description`,
+      deadline: new Date(),
+      tasks: [],
+    };
+
+    this.projectService.addProject(newProject);
+  }
+
+  viewProject(projectId: number): void{
+    // Navigate to be implemented
+  }
 }
